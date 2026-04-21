1d17b2435fcd1d71818f773acee950a6204b90ee
David Jose Ferreira do Nascimento <dvdjoseferreira@gmail.com>
2026-04-21 18:36:40 -0300

    Create variaveis.kt

---
+++ variaveis.kt
@@ -0,0 +1,10 @@
+/**
+ * Declarando variaveis!
+ * **/
+fun main(){
+    //Var vem de variable
+    var teste: String = "Hello Wolrd!"
+    var t1 = "Hello Wolrd!"
+    println(teste)
+    println(t1)
+}
