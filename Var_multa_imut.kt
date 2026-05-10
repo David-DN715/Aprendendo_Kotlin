d52745d56daf13d82aeac9809a82ef59d6cb2031
David Jose Ferreira do Nascimento <dvdjoseferreira@gmail.com>
2026-05-10 18:08:29 -0300

    Create Var_multa_imut.kt

---
+++ Var_multa_imut.kt
@@ -0,0 +1,12 @@
+/**
+ * Variais multaveis e imultaveis!
+ * val não vai ser nunca um valor modificavel!
+ * var vai ser um valor modificavel!
+ * **/
+fun main(){
+    var str:String="My name is..."
+    val pri:String="Não vou ser alterado!"
+    println(str)
+    println("variavel não multavel:\n")
+    println(pri)
+}
